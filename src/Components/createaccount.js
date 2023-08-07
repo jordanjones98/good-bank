5fda1cb2b03bd8ea0794be8f26fc315fe32ce91a
Jordan Jones <me@jjones.tech>
2023-08-06 19:40:32 -0500

    take out unneeded stuff

--- src/Components/createaccount.js
+++ src/Components/createaccount.js
@@ -1,13 +1,11 @@
 import React, { useState, useContext } from "react";
 import Structure from "./structure";
 import { UserContext } from "../Context/UserContext";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const CreateAccount = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [success, setSuccess] = useState(false);
   const userContext = useContext(UserContext);
 
   const handleNameChange = (event) => {
@@ -36,32 +34,12 @@ const CreateAccount = () => {
       try {
         userContext.createAccount(name, email, password);
       } catch (error) {
-        setSuccess(false);
         console.log(error);
         alert("Error creating account");
       }
-
-      setSuccess(true);
     }
   };
 
-  const saveUserInStore = async (email, password) => {
-    const auth = getAuth();
-    try {
-      const user = await createUserWithEmailAndPassword(auth, email, password);
-      return user.user;
-    } catch (error) {
-      console.log("Error creating user", error.code, error.message);
-      throw error;
-    }
-  };
-
-  const handleAddAnotherAccount = () => {
-    setName("");
-    setEmail("");
-    setPassword("");
-    setSuccess(false);
-  };
   return (
     <Structure>
       <div className="container mt-5">
@@ -69,78 +47,65 @@ const CreateAccount = () => {
           <div className="col-md-6">
             <div className="card">
               <div className="card-body">
-                {success ? (
-                  <>
-                    <h3 className="card-title text-center">
-                      Account created successfully
-                    </h3>
+                <>
+                  <h4
+                    className="card-title text-center"
+                    style={{ textDecoration: "underline" }}
+                  >
+                    Create Account
+                  </h4>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="nameInput">Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="nameInput"
+                        placeholder="Name"
+                        value={name}
+                        onChange={handleNameChange}
+                      />
+                    </div>
+                    <div className="form-group mt-2">
+                      <label htmlFor="emailInput">Email</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="emailInput"
+                        placeholder="Email"
+                        value={email}
+                        onChange={handleEmailChange}
+                      />
+                    </div>
+                    <div className="form-group mt-2">
+                      <label htmlFor="passwordInput">Password</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="passwordInput"
+                        placeholder="Password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                      />
+                    </div>
+
+                    {password && password.length < 8 && (
+                      <div className="alert alert-info mt-2">
+                        Password must be 8 characters long
+                      </div>
+                    )}
+
                     <button
-                      className="btn btn-dark mt-3"
-                      onClick={handleAddAnotherAccount}
-                    >
-                      Add Another Account
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    <h4
-                      className="card-title text-center"
-                      style={{ textDecoration: "underline" }}
+                      type="submit"
+                      className="btn btn-dark mt-2"
+                      disabled={
+                        !name || !email || !password || password.length < 8
+                      }
                     >
                       Create Account
-                    </h4>
-                    <form onSubmit={handleSubmit}>
-                      <div className="form-group">
-                        <label htmlFor="nameInput">Name</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="nameInput"
-                          placeholder="Name"
-                          value={name}
-                          onChange={handleNameChange}
-                        />
-                      </div>
-                      <div className="form-group mt-2">
-                        <label htmlFor="emailInput">Email</label>
-                        <input
-                          type="email"
-                          className="form-control"
-                          id="emailInput"
-                          placeholder="Email"
-                          value={email}
-                          onChange={handleEmailChange}
-                        />
-                      </div>
-                      <div className="form-group mt-2">
-                        <label htmlFor="passwordInput">Password</label>
-                        <input
-                          type="password"
-                          className="form-control"
-                          id="passwordInput"
-                          placeholder="Password"
-                          value={password}
-                          onChange={handlePasswordChange}
-                        />
-                      </div>
-
-                      {password && password.length < 8 && (
-                        <div className="alert alert-info mt-2">
-                          Password must be 8 characters long
-                        </div>
-                      )}
-
-                      <button
-                        type="submit"
-                        className="btn btn-dark mt-2"
-                        disabled={
-                          !name || !email || !password || password.length < 8
-                        }
-                      >
-                        Create Account
-                      </button>
-                    </form>
-                  </>
+                    </button>
+                  </form>
+                </>
                 )}
               </div>
             </div>
